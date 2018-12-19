218d686314102da7b381948223f640668631ae52
willy7728 <107021050@gm.asia.edu.tw>
2018-12-19 15:53:53 +0800

    fin new call and random

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,65 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Scanner scn =new Scanner(System.in);
-	    //01
+	    //
 //        int n=scn.nextInt(),m=scn.nextInt();
 //        System.out.println(c(n)/(c(m)*c(n-m)));
-        //02
+        //
 //        k();
         //
+//        int n=scn.nextInt();
+//        System.out.println(count(n));
+        //
+//        int n=scn.nextInt();
+//        String str =scn.next();
+//        tr(n,str);
+        //01
+//        String str=scn.next();
+//        for (int i=str.length()-1;i>=0;i--){
+//            System.out.print(str.charAt(i));
+//        }
+        //02
         int n=scn.nextInt();
-        System.out.println(count(n));
+        Random randnum =new Random();
+        randnum.setSeed(n);
+        int[] data=new int[6];
+        int j=0;
+        boolean flag=true;
+        while (j<6){
+            flag=true;
+            data[j]=randnum.nextInt(42)+1;
+            for (int k=0;k<j;k++){
+                if (data[k]==data[j]){
+                    flag=false;
+                }
+            }
+            if (flag==true){
+                j++;
+            }
+        }
+        for (int i=0;i<6;i++){
+            System.out.print(data[i]+"\t");
+        }
+        System.out.println();
+
+        //03
+//        int[][] data=new int[5][7];
+//        for (int i=0;i<5;i++){
+//            for (int j=0;j<7;j++){
+//                data[i][j]=scn.nextInt();
+//            }
+//        }
+//        for (int i=0;i<7;i++){
+//            for (int j=0;j<5;j++){
+//                System.out.print(data[j][i]+"\t");
+//            }
+//            System.out.println();
+//        }
+
     }
     public static int c(int n) {
         int sum=1;
@@ -33,5 +81,14 @@ public class Main {
            t++;
        }
        return t;
+    }
+    public static void tr(int n,String str){
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(str);
+            }
+            System.out.println();
+        }
+
     }
 }
